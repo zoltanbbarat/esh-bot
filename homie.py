d7684d04d810cf173c78f38032e42859782420b5
zack barat <zackb@PumpieMachine.>
2023-11-23 19:53:36 +0100

    Added class, error handling, logging

--- homie.py
+++ homie.py
@@ -1,19 +1,44 @@
 import os
 import random
+import re
+import logging
 
 import discord
 from discord.ext import commands
 
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix="/", intents=intents)
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
-games = ["cs2", "battlebit", "aoe", "lol", "wot"]
-
-
-@bot.command()
-async def game(ctx):
-    await ctx.send(random.choice(games))
+game_list = ["cs2", "battlebit", "aoe", "lol", "wot"]
 
 
-bot.run(os.environ["HOMIE_TOKEN"])
+class EmotionalSupportHomieBot(commands.Bot):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__("/", intents=intents)
+
+        @self.command()
+        async def game(ctx):
+            logging.info(f"Game command called by {ctx.author}")
+
+            exclude_list = re.findall(r"(?<=-)[^\s]+", ctx.message.content)
+            random_game = random.choice([x for x in game_list if x not in exclude_list])
+
+            try:
+                await ctx.send(random_game)
+            except Exception as e:
+                logging.error(e)
+
+    async def on_ready(self):
+        logging.info(f"We have logged in as {self.user}")
+
+
+if __name__ == "__main__":
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    try:
+        esh = EmotionalSupportHomieBot(intents=intents)
+        esh.run(os.environ["HOMIE_TOKEN"])
+    except Exception as e:
+        logging.error(e)
